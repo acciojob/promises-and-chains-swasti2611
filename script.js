96a34403706c57d58268eef57b554aa678be0449
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-15 16:04:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,67 +1,24 @@
 //your JS code here. If required.
-// let btn=document.getElementById(btn);
-// function promisefun(age,name){
-// 	return new promise((resolve,reject)=>{
-// 		if (age>18) {
-// 			setTimeout(() => {
-//             resolve(
-//                 alert(`welcome , ${name}, You can vote`)
-//                 // console.log(`You can vote , ${name}`)
-//             )
-//         } , 1000)
-			
-// 		}else{
-// 			settimeout(()=>{
-// 				reject(alert(`Oh sorry ${name}. You aren't old enough.`));
-// 			},4000)
-			
-			
-// 		}
-// 	}
-// }
+let btn=document.getElementById("btn");
 
 
-// btn.addEventListner("click",()=>{
-// 	let age=document.getElementById( "age").value;
-// let name=document.getElementById( "name").value;
+btn.addEventListner("click",()=>{
+	let age=document.getElementById( "age").value;
+    let name=document.getElementById( "name").value;
 
-// 	if(age==" " &&  name==" " ){
-// 		alret("plsese fill the age ang name");
-// 	}else{
-// 		promisefun(age,name);
-// 	}
-// })
 
-letet formbutton = document.getElementById("btn");
-function promiseApi1(name , age)
-{
- return new Promise((resolve , reject) => {
-    if(age > 18)
-    {
-        setTimeout(() => {
-            resolve(
-                alert(`welcome , ${name}, You can vote`)
-                // console.log(`You can vote , ${name}`)
-            )
-        } , 4000)
-    }
-    else
-    {
-        alert(`Oh sorry ${name}. You aren't old enough.`)
-    }
- })
-}
-formbutton.addEventListener("click" , (event) => {
-    event.preventDefault();
-    let nameinput = document.getElementById("name").value;
-    let ageinput = document.getElementById("age").value;
-    if (nameinput == "" && ageinput == "") {
-        alert("It should not be empty")
-    }
-    else
-    {
-        promiseApi1(nameinput , ageinput);
-    }
-})
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>18){
+			alert(`Welcome, ${name}. You can vote.`)
+			}
+			else{
+				alert(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		},4000)
+	})
+});
+
+
 	
 	
